f6a882ee868f8e8487c4a0b155d6ddb91b5d9273
vito <victorgpj25@gmail.com>
2022-06-16 20:22:34 -0300

    Tweets Get

--- back/index.js
+++ back/index.js
@@ -2,8 +2,6 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 
-
-
 const app = express()
 app.use(bodyParser.json());
 app.use(cors())
@@ -11,6 +9,17 @@ app.listen(5000);
 
 const users = []
 const tweets = []
+let latestTweets = []
+let displayTweets = []
+
+function addAvatar(tweetUsername, tweetContent) {
+    let userData = users.filter( user => user.username === tweetUsername )[0]
+    displayTweets.push({
+        username: tweetUsername,
+        avatar: userData.avatar,
+        tweet: tweetContent
+    })
+}
 
 app.post('/sign-up', (req, res) => {
     users.push(req.body);
@@ -20,5 +29,12 @@ app.post('/sign-up', (req, res) => {
 app.post('/tweets', (req, res) => {
     tweets.push(req.body);
     res.send("OK");
+});
+
+app.get('/tweets', (req, res) => {
+    displayTweets = []
+    latestTweets = tweets.slice(-10)
+    latestTweets.map( tweet => addAvatar(tweet.username, tweet.tweet) )
+    res.send(displayTweets.reverse()); // reverse() para colocar os tweets mais novos no topo
 });
 
